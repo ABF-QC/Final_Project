43c5dd891b1de4017ae37c739cc6d0064a555fb7
Anna-Belle Filion <treblecharger34@hotmail.com>
2025-03-31 16:18:34 -0400

    data preprocessing in progres

--- notebook/id_station.py
+++ notebook/id_station.py
@@ -1,5 +1,5 @@
 stn_id = {
-    'Montreal (McTavish/McGill)' : [7025280,
-                                    7024745]
+    'Montreal (McTavish/McGill)' : ['7025280',
+                                    '7024745']
     
 }
